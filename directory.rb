2d4c9779524c900b93536ea14277751cf14d185f
Prabu Sathan <prabu.sathan@gmail.com>
2017-04-28 23:53:35 +0100

    Methods

--- directory.rb
+++ directory.rb
@@ -12,12 +12,21 @@ students = ["Dr Hannibal Lecter",
             "Joffrey Baratheon",
             "Norman Bates"]
 
-
+def print_header
 puts "The students of Villains Academy"
 puts "-------------"
-
-students.each do |student|
-  puts student
 end
 
-puts "Overall, we have #{students.count} great students"
+def print(names)
+names.each do |name|
+  puts name
+  end
+end
+
+def print_footer(names)
+puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
